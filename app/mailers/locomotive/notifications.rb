77f6f471f6282265f064c791fa968b7564508bf0
Abhilash Hebbar <abhi.h@openly.co>
2014-03-24 08:52:36 +0530

    Customer message support for Currency cloud

--- app/mailers/locomotive/notifications.rb
+++ app/mailers/locomotive/notifications.rb
@@ -13,8 +13,26 @@ module Locomotive
       end
 
       subject = t('locomotive.notifications.new_content_entry.subject', domain: @domain, type: @type.name, locale: account.locale)
+      to = account.email
 
-      mail subject: subject, to: account.email
+
+      if @type.slug == 'customer_messages'
+        subject = "#{@entry.name} has left a message at currency cloud portal(#{entry.department} department)."
+        case @entry.department
+        when 'Accounts'
+          to = ACCOUNT_DEPT_EMAIL
+        when 'Marketing'
+          to = MARKETING_DEPT_EMAIL
+        when 'Press Office'
+          to = PRESS_OFFICE_DEPT_EMAIL
+        when 'Sales'
+          to = SALES_DEPT_EMAIL
+        when 'Tech Team'
+          to = TECH_DEPT_EMAIL
+        end
+      end
+
+      mail subject: subject, to: to
     end
   end
 
